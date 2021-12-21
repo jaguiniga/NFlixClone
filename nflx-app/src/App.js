7fa184cff469aba8b46e28f20f0b915c1b6def5f
jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-20 20:39:10 -0800

    Added the basic setup for Nflx Clone React App
    Basic npx create-react-app setup + some file cleanup

---
+++ nflx-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello! This is our basic app setup! Cheers!</h1>
+    </div>
+  );
+}
+
+export default App;
