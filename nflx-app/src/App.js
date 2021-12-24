ec3d682b7fc346bd3f4e7d6a35dad8bfb02e90c6
jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-23 21:04:35 -0800

    Finishing rows
    Done with rows!

--- nflx-app/src/App.js
+++ nflx-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello! This is our basic app setup! Cheers!</h1>
+      {/* Nav */}
+      {/* Banner */}
       <Row 
         title="NETFLIX ORIGINALS" 
         fetchURL={requests.fetchNetflixOriginals}
---
+++ nflx-app/src/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async funtion fetchData() {
+            // 
+        }
+        fetchData()
+    }, []);
+
+
+    return (
+        <header> {/*<<< Background image*/}
+            {/* Title */}
+            {/*div for 2 buttons*/}
+            {/*description*/}
+        </header>
+    )
+}
+
+export default Banner
