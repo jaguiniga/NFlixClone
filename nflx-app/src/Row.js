fc1f038880f847b4e1a8b06803dd06f68c924f15
jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-23 20:16:38 -0800

    Row implementation
    Finally got rows together and working!
    Not totally finished yet but I am making progress

--- nflx-app/src/Row.js
+++ nflx-app/src/Row.js
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL }) {
     const [movies, setMovies] = useState([]);
     
     // conditional snippet
-        useEffect(() => {
+    useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchURL);
-            console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-        }, []);
+    }, [fetchURL]);
 
-    
+    console.table(movies);
+
     return (
-        <div>
-            <h2>{title}</h2>
+        <div className="row">
+          <h2>{title}</h2>
 
-            {/* container -> posters */}
+          <div className="row__posters">
+            {/* several row__poster(s) */}
+
+            {movies.map((movie) => (
+              <img 
+                className="row__poster"
+                src={`${base_url}${movie.poster_path}`} 
+                alt={movie.name}
+                />
+            ))}
+            </div>
         </div>
     )
 }
