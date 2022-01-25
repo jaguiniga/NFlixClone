7e65a37c424c40886155201036668aa6b933659d
jurgden <82841924+jurgden@users.noreply.github.com>
2022-01-25 15:22:14 -0800

    Fixed index routing

--- nflx-app/src/App.js
+++ nflx-app/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Nav from "./Nav";
+import { Routes, Route } from "react-router-dom";
 import Main from "./Main";
-
-
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div className="app">
       <Nav />
-      <Main />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
--- nflx-app/src/Nav.js
+++ nflx-app/src/Nav.js
@@ -1,59 +1,47 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link
-  } from "react-router-dom";
-import React, { useState, useEffect } from 'react';
-import './Nav.css';
-import Signup from './components/Signup';
-import Main from "./Main"
-   // import FormSignup from "./components/FormSignup";
-   // import useForm from "./components/useForm";
-   // import SignupSuccess from "./components/SignupSuccess";
-   // import validateInfo from "./components/validateInfo";
+import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+// import FormSignup from "./components/FormSignup";
+// import useForm from "./components/useForm";
+// import SignupSuccess from "./components/SignupSuccess";
+// import validateInfo from "./components/validateInfo";
 
 function Nav() {
-    const [show, handleShow] = useState(false);
-    
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            } else handleShow(false);
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
-    }, []);
+  const [show, handleShow] = useState(false);
 
-    return (
-        <Router>
-            <div className={`nav ${show && "nav_black"}`}>
-                <img 
-                className="nav_logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
-                alt="Netflix Logo"
-                />
-                <Link to="/Signup">
-                    <img
-                    className="nav_key"
-                    src='https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-key-thin.png'
-                    alt="Netflix Signup"
-                    />
-                </Link>
-                <Routes>
-                    <Route path="/" element={ <Main/> } exact />
-                    <Route path="/signup" element={ <Signup /> } />
-                </Routes>
-                <img
-                className="nav_avatar"
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" 
-                alt="Netflix Logo"
-                />
-             </div>
-        </Router>
-    )
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+      <NavLink to="/signup">
+        <img
+          className="nav_key"
+          src="https://cdns.iconmonstr.com/wp-content/assets/preview/2018/240/iconmonstr-key-thin.png"
+          alt="Netflix Signup"
+        />
+      </NavLink>
+
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
