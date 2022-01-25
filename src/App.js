8d03d0dbd5f61f41d827e71cea2be7c98e9948a6
jurgden <82841924+jurgden@users.noreply.github.com>
2022-01-25 15:58:37 -0800

    Cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import './App.css';
-import requests from './requests';
-import Row from './Row';
-import Banner from './Banner';
+import React from "react";
+import "./App.css";
 import Nav from "./Nav";
-import LoginForm from './components/LoginForm';
-import { Route, Routes, BrowserRouter} from 'react-router-dom';
-import Practice from './components/Practice';
-import Main from './components/Main';
-
+import LoginForm from "./components/LoginForm";
+import { Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="app">
-      
-
       <Nav />
-      
+
       <Routes>
         <Route path="/" />
-        <Route path="/login" element = { <LoginForm/> } />
-        <Route path="/main" element = { <Main/>} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/main" element={<Main />} />
       </Routes>
-      
-     
-
     </div>
   );
 }
